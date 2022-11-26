c07ac2e99219742624240766e7fdde9ab6a66fa1
ssds <dorosh.kostya65@gmail.com>
2022-11-26 03:13:52 +0200

    Added progress spinner

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,7 +1,7 @@
 import { APP_INITIALIZER, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
-import { HttpClient, HttpClientModule } from "@angular/common/http";
+import { HTTP_INTERCEPTORS, HttpClient, HttpClientModule } from "@angular/common/http";
 import { BaseHttpService } from "@app/services/base-http.service";
 import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 import { TranslateHttpLoader } from "@ngx-translate/http-loader";
@@ -9,6 +9,9 @@ import { TranslateLoader, TranslateModule, TranslateService } from "@ngx-transla
 import { appInitializerFactory } from "@app/services/initializer.factory";
 import { AppRoutingModule } from "./app-routing.module";
 import { HomeModule } from "@app/module/home.module";
+import { SpinnerService } from "@app/services/spinner.service";
+import { SpinnerModule } from "./components/spinner/spinner.module";
+import { SpinnerInterceptor } from "./interceptors/spinner.interceptor";
 
 const createTranslateLoader = (http: HttpClient): TranslateHttpLoader => {
   return new TranslateHttpLoader(http, './assets/i18n/', '.json');
@@ -33,9 +36,12 @@ const createTranslateLoader = (http: HttpClient): TranslateHttpLoader => {
       },
     }),
     HomeModule,
+    SpinnerModule,
   ],
   providers: [
     BaseHttpService,
+    SpinnerService,
+    { provide: HTTP_INTERCEPTORS, useClass: SpinnerInterceptor, multi: true },
     {
       provide: APP_INITIALIZER,
       useFactory: appInitializerFactory,
